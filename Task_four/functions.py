605fa76bfb9445677acb0c3760e4995fcfd44453
dimastarobruanskiy <dima.starobruanskiy@gmail.com>
2026-04-15 22:16:52 +0300

    Add error handling decorator for command functions

---
+++ Task_four/functions.py
@@ -0,0 +1,44 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Enter user name."
+        except KeyError:
+            return "Contact not found."
+    return inner
+
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+    name, new_phone = args
+    contacts[name]
+    contacts[name] = new_phone
+    return "Contact updated."
+
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    phone = contacts[name]
+    return f"{name}: {phone}"
+
+def show_all(contacts):
+    if not contacts:
+        return "No contacts found."
+    result = ""
+    for name, phone in contacts.items():
+        result += f"{name}: {phone}\n"
+    return result.strip()
+
